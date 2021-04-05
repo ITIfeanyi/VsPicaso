728f1882b99758995a744b173be0294624c41858
ITIfeanyi <ifeanyithankgodigweh@gmail.com>
2021-04-05 15:45:19 -0700

    added extra sub nav & change the header background-color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,82 @@
 import React, { useState } from "react";
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faTimes,
+  faAngleDown,
+  faAngleUp,
+} from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   const [navOpen, setNavOpen] = useState(false);
+  const [packageOpen, setPackageOpen] = useState(false);
 
   const handleNavOpen = () => {
     setNavOpen(!navOpen);
   };
-
+  const handlePackage = () => {
+    setPackageOpen(!packageOpen);
+  };
   let menu;
   if (navOpen) {
     menu = (
       <div className='nav_container'>
         <div className='nav_subcontainer'>
           <ul className='menu_container'>
-            <li className='menu_list'>
+            <li className='menu_list' onClick={handleNavOpen}>
               <Link className='menu_link' to='/'>
                 Home
               </Link>{" "}
             </li>
-            <li className='menu_list'>
+            <li className='menu_list' onClick={handleNavOpen}>
               <Link className='menu_link' to='/explore'>
                 Explore
               </Link>{" "}
             </li>
+
             <li className='menu_list'>
+              <p className='menu_link_p' onClick={handlePackage}>
+                Package
+                {packageOpen ? (
+                  <FontAwesomeIcon
+                    icon={faAngleUp}
+                    className='menu_link_icon'
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    icon={faAngleDown}
+                    className='menu_link_icon'
+                  />
+                )}
+              </p>
+            </li>
+            {packageOpen && (
+              <ul>
+                <li className='menu_list' onClick={handleNavOpen}>
+                  <Link className='menu_link subPackageLink' to='/wedding'>
+                    Wedding
+                  </Link>{" "}
+                </li>
+                <li className='menu_list' onClick={handleNavOpen}>
+                  <Link className='menu_link subPackageLink' to='/birthdays'>
+                    Birthdays
+                  </Link>{" "}
+                </li>
+                <li className='menu_list' onClick={handleNavOpen}>
+                  <Link className='menu_link subPackageLink' to='/studio-shoot'>
+                    Studio Shoot
+                  </Link>{" "}
+                </li>
+              </ul>
+            )}
+            <li className='menu_list' onClick={handleNavOpen}>
               <Link className='menu_link' to='/contact'>
                 Contact
               </Link>{" "}
             </li>
-            <li className='menu_list'>
+            <li className='menu_list' onClick={handleNavOpen}>
               <Link className='menu_link' to='/project'>
                 Projects
               </Link>{" "}
